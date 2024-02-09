fb85698eb7fab9c644718fa414746322ded17a25
alexhtalbot <158988480+alexhtalbot@users.noreply.github.com>
2024-02-09 02:31:35 +0000

    Updated py file

--- lesson2.py
+++ lesson2.py
@@ -1,2 +1,6 @@
 message="BeepBoop!"
-print(message)
+message_upper=message.upper()
+print(5*message_upper)
+print(len(5*message_upper))
+print('My annoying text is ' + 5*message_upper)
+print('It has 45 characters')
