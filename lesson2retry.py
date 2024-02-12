8e38a8d02389ef79077b9b5de362c539183caf7d
alexhtalbot <158988480+alexhtalbot@users.noreply.github.com>
2024-02-12 05:33:53 +0000

    New lesson 2 code

---
+++ lesson2retry.py
@@ -0,0 +1,7 @@
+text='BeepBoop! '
+text_echo=text*10
+print(text_echo)
+print(len(text_echo))
+print(text_echo.upper())
+print(f"My annoying text is:{text_echo.upper()}")
+print(f"My annoying text is: {text_echo.upper()}" + f"It has {len(text_echo)} characters")
