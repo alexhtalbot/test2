d59f06f683e41505f997ed26da21ca83bcce4338
alexhtalbot <158988480+alexhtalbot@users.noreply.github.com>
2024-03-08 03:54:24 +0000

    slight changes to lesson 4

--- lesson4.py
+++ lesson4.py
@@ -1,4 +1,4 @@
-palindrome_word = input("Please enter a word: ")
+palindrome_word = input("Please enter a word to see if it's a palindrome: ")
 palindrome_word_list = list(palindrome_word)
 palindrome_word_list_reversed = palindrome_word_list[::-1]
 
@@ -6,4 +6,4 @@ if palindrome_word_list == palindrome_word_list_reversed:
   print("This is a palindrome")
 
 else:
-  print("This is not a palindrome")
+  print("This is not a palindrome, try again")
