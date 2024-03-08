063cef0577c43d06e83a9d33020fb4a375f15ee2
alexhtalbot <158988480+alexhtalbot@users.noreply.github.com>
2024-03-08 05:13:05 +0000

    slightly modified lesson 4

--- lesson4.py
+++ lesson4.py
@@ -3,7 +3,7 @@ palindrome_word_list = list(palindrome_word)
 palindrome_word_list_reversed = palindrome_word_list[::-1]
 
 if palindrome_word_list == palindrome_word_list_reversed:
-  print("This is a palindrome")
+  print("True: this is a palindrome")
 
 else:
-  print("This is not a palindrome, try again")
+  print("False: this is not a palindrome, try again")
